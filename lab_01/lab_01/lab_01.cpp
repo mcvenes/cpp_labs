5b1ad13f1a07dcf0209bd974287dfcb47e9491e8
mcvenes <mila-gygy@mail.ru>
2022-03-26 00:30:44 +0300

    Added code to check stack's operations and exceptions

--- lab_01/lab_01/lab_01.cpp
+++ lab_01/lab_01/lab_01.cpp
@@ -6,11 +6,40 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::cout << "Hello World!\n";
 
-    MyStack<std::string> stack(3);
+	MyStack<std::string> stack(3);
 
-    std::cout << "stack size: " << stack.size() << ", max size: " << stack.capacity() << std::endl;
+	std::cout << "Trying to insert 5 elements to stack with capacity " << stack.capacity() << std::endl;
+	for (int i = 0; i < 5; i++)
+	{
+		std::string str = "test string N_" + std::to_string(i);
 
-    return 0;
+		try
+		{
+			stack.push(str);
+			std::cout << "Insert value: \"" << str << "\" to stack done, current stack size : "
+				<< stack.size() << " / " << stack.capacity() << std::endl;
+		}
+		catch (const Exc::EStackOverflow& e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+	}
+
+	std::cout << "Trying read 5 values from stack with capacity " << stack.capacity() << std::endl;
+	for (int i = 0; i < 5; i++)
+	{
+		try
+		{
+			std::string str = stack.pop();
+			std::cout << "Reading done, value: \"" << str << "\"" << std::endl;
+		}
+		catch (const Exc::EStackEmpty& e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+	}
+
+	return 0;
 }
